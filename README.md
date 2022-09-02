510da6a2c4d8298e0116d713a7b0e27d1a7d54e4
Daud Dhiya <daud.dhiya31@gmail.com>
2022-09-02 15:08:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## freegames-python
+
+all this code sourced from : https://pypi.org/project/freegames/
+
+first, install the py-library for freegames `pip install freegames`
---
+++ snake.py
@@ -0,0 +1,8 @@
+# https://www.youtube.com/watch?v=BKwnOOs0ml8
+
+from turtle import *
+from random import randrange
+from freegames import square, vector
+
+food = vector(0,0)
+snake = [vector(10,0)]
